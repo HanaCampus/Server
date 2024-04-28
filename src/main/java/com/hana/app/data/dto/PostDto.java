ef213574d259185fb102bcbb1b78fc1cd7fa9663
TakGyun <gyun1712@gmail.com>
2024-04-29 00:54:09 +0900

    feat: Post Dto 생성

---
+++ src/main/java/com/hana/app/data/dto/PostDto.java
@@ -0,0 +1,25 @@
+package com.hana.app.data.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class PostDto {
+    private int postId;
+    private String title;
+    private String content;
+    private boolean isAnonymous;
+    private int scraps;
+    private int likes;
+    private int userId;
+    private int boardId;
+    private LocalDate createDate;
+    private LocalDate updateDate;
+}
