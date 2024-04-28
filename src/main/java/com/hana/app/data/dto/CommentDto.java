40a7e37497bfa9543ddcb0d8c35c62ae1eb336cd
LimEunSang <dmstkd2905@naver.com>
2024-04-28 21:48:29 +0900

    feat: comment dto 생성

---
+++ src/main/java/com/hana/app/data/dto/CommentDto.java
@@ -0,0 +1,20 @@
+package com.hana.app.data.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class CommentDto {
+    private int commentId;
+    private String content;
+    private int likes; // 좋아요 수
+    private String status; //
+    private int userId;
+    private int postId;
+    private int parentId; // 대댓글일 경우 부모 댓글의 id
+}
