e7a32c8afb75686c06545a94b95e8af9fb1642fa
yunred <nado0903@gmail.com>
2024-04-30 17:05:23 +0900

    feat: SecurityConfig 설정

---
+++ src/main/java/com/hana/app/config/SecurityConfig.java
@@ -0,0 +1,48 @@
+package com.hana.app.config;
+
+import lombok.AllArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+@Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(securedEnabled = true, prePostEnabled = true)
+@AllArgsConstructor
+public class SecurityConfig  {
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        // CSRF 설정 Disable
+        http
+            .csrf((csrf) ->
+                csrf.disable()
+            )
+            .logout((logoutConfig) ->
+                //로그아웃
+                logoutConfig.logoutSuccessUrl("/")
+            );
+
+        // CORS 설정
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.addAllowedOrigin(CorsConfiguration.ALL);
+        configuration.addAllowedMethod(CorsConfiguration.ALL);
+        configuration.addAllowedHeader(CorsConfiguration.ALL);
+
+        // 권한 규칙 작성
+        http.authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers("/", "/login").permitAll()
+//                        .requestMatchers("/admin").hasRole("ADMIN")
+                        .anyRequest().permitAll()
+//                        .anyRequest().authenticated()
+        );
+        return http.build();
+    }
+
+
+}
