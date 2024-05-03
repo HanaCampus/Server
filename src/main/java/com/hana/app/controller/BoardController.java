a586bf126918dc3121b26a0e917382b8c071174b
LimEunSang <dmstkd2905@naver.com>
2024-05-03 13:40:13 +0900

    feat: board detail page publishing 작업 중..
    - board detail page publishing 작업 중
    - push를 위한 임시 commit

---
+++ src/main/java/com/hana/app/controller/BoardController.java
@@ -0,0 +1,33 @@
+package com.hana.app.controller;
+
+import com.hana.app.data.dto.UserDto;
+import com.hana.app.service.BoardService;
+import jakarta.servlet.http.HttpSession;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+@RequestMapping("/boards")
+@RequiredArgsConstructor
+@Slf4j
+public class BoardController {
+
+    final BoardService boardService;
+
+    String dir = "boards/";
+
+    @GetMapping("")
+    public String userMyPage(Model model, @RequestParam("id") Integer boardId) {
+        // 자유게시판
+        if(boardId == 1) {
+            model.addAttribute("center", dir + "free");
+        }
+
+        return "index";
+    }
+
+
+}
