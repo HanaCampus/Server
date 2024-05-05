3f56a2b575fad1c2feb4593e4db5b422a1fa0168
TakGyun <gyun1712@gmail.com>
2024-05-06 03:23:37 +0900

    feat: 삭제된 댓글 개수 Count 제외 및 Scrap Count 로직 추가

--- src/main/java/com/hana/app/repository/PostRepository.java
+++ src/main/java/com/hana/app/repository/PostRepository.java
@@ -19,5 +19,6 @@ public interface PostRepository extends HanaRepository<Integer, PostDto> {
     int insertByAnonymous(PostDto postDto) throws Exception;
     int insertByNotAnonymous(PostDto postDto) throws Exception;
     int updateLikeCount(Integer id) throws Exception;
+    int updateScrapCount(Integer id) throws Exception;
 
 }
